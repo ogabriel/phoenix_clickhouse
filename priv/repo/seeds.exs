0a33a1302195a51c24f21ef35c62ac47b1f5ed74
ogabriel <gabrieloliver8991@gmail>
2024-09-20 13:07:04 -0300

    add seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,28 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Click.Cars.Car
+alias Click.Repo
+
+drivetrain = {"AWD", "RWD", "FWD"}
+transmission = {"Manual", "Automatic"}
+
+rows =
+  Enum.map(1..1000, fn _ ->
+    brand = Faker.Vehicle.make()
+
+    %{
+      name: Faker.Vehicle.model(brand),
+      brand: brand,
+      drivetrain: elem(drivetrain, Enum.random(0..2)),
+      transmission: elem(transmission, Enum.random(0..1)),
+      fuel_type: Faker.Vehicle.fuel_type(),
+      body_style: Faker.Vehicle.body_style(),
+      horse_power: Enum.random(1..2000),
+      weight: Enum.random(1..10000),
+      options: Faker.Vehicle.options()
+    }
+  end)
+
+Repo.insert_all(Car, rows)
