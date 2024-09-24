cd0b171012020b3baddb46a16b9ee55a9e916170
ogabriel <gabrieloliver8991@gmail.com>
2024-09-24 17:30:28 -0300

    add create operation

--- lib/click_web/controllers/car_controller.ex
+++ lib/click_web/controllers/car_controller.ex
@@ -1,15 +1,26 @@
 defmodule ClickWeb.CarController do
   use ClickWeb, :controller
+  use OpenApiSpex.ControllerSpecs
 
   alias Click.Cars
   alias Click.Cars.Car
+  alias ClickWeb.Schemas.Car
 
   action_fallback ClickWeb.FallbackController
+
+  operation :index, false
 
   def index(conn, _params) do
     cars = Cars.list_cars()
     render(conn, :index, cars: cars)
   end
+
+  operation :create,
+    summary: "Create car",
+    request_body: {"Car params", "application/json", Car},
+    responses: [
+      created: {"Car response", "application/json", Car}
+    ]
 
   def create(conn, %{"car" => car_params}) do
     with {:ok, %Car{} = car} <- Cars.create_car(car_params) do
